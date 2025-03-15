3707b0e16d8334a357d376e312efedc5188fea7e
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-03-15 16:51:50 +0000

    Install Django v5.1.1

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d7pi9!m_57$=-pq6qfl#4jgw@#(9#6t-f4gvji89p6t1nd4d(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
