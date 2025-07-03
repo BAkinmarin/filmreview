c70bf471dd3321627ec857827bc7f5285b49fe4b
Bola Akinmarin <bolakinmarin@yahoo.co.uk>
2025-07-03 23:04:47 +0100

    Update review views

--- review/views.py
+++ review/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import CommentForm
-
-# Create your views here
+from django.db.models import Q
 
 
 class PostList(generic.ListView):
